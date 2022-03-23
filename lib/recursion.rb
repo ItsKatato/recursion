7b037b3e46c35f441023d0e46720915e99b5ddf4
ItsKatato <kaelahsmallwood@gmail.com>
2022-03-23 11:14:23 -0500

    Removed sort_half

--- lib/recursion.rb
+++ lib/recursion.rb
@@ -1,26 +1,5 @@
 # frozen_string_literal: true
 
-def sort_half(array, merged = [])
-  return merged if array.nil? || array.empty?
-
-  if array.length > 1
-    case array[0] <=> array[1]
-    when 1
-      merged.push(array.delete_at(1))
-      sort_half(array, merged)
-    when -1
-      merged.push(array.shift)
-      sort_half(array, merged)
-    when 0
-      merged.push(array.shift)
-      sort_half(array, merged)
-    end
-  else
-    merged.push(array.shift)
-    sort_half(array, merged)
-  end
-end
-
 def sort_whole(left, right, merged = [])
   return merged if left.empty? && right.empty?
 
@@ -77,11 +56,11 @@ def merge_sort(num)
   if num.length < 2
     num
   else
-    l_half = sort_half(merge_sort(num.take(num.length / 2)))
-    r_half = sort_half(merge_sort(num.drop(num.length / 2)))
+    l_half = merge_sort(num.take(num.length / 2))
+    r_half = merge_sort(num.drop(num.length / 2))
     sort_whole(l_half, r_half)
   end
 end
 
-# p merge_sort([14, 13, 16, 19, 1, 2])
-p merge_sort([18, 14, 11, 1, 2, 5, 4, 16, 8, 9])
+p merge_sort([14, 13, 16, 19, 1, 2])
+# p merge_sort([18, 14, 11, 1, 2, 5, 4, 16, 8, 9])
