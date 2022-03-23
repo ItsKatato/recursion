507c883fe0b7b1eae8664f0c79b7d8bfbd1d9e72
ItsKatato <kaelahsmallwood@gmail.com>
2022-03-23 11:20:23 -0500

    Cleaned code

--- lib/recursion.rb
+++ lib/recursion.rb
@@ -1,57 +1,5 @@
 # frozen_string_literal: true
 
-# def sort_whole(left, right, merged = [])
-#   return merged if left.empty? && right.empty?
-
-#   if left.length >= 1 && right.length >= 1
-#     case left[0] <=> right[0]
-#     when 1
-#       merged.push(right.shift)
-#       sort_whole(left, right, merged)
-#     when -1
-#       merged.push(left.shift)
-#       sort_whole(left, right, merged)
-#     when 0
-#       merged.push(left.shift)
-#       sort_whole(left, right, merged)
-#     end
-#   elsif left.empty? && right.length.positive?
-#     if right.length > 1
-#       case right[0] <=> right[1]
-#       when 1
-#         merged.push(right.delete_at(1))
-#         sort_whole(left, right, merged)
-#       when -1
-#         merged.push(right.shift)
-#         sort_whole(left, right, merged)
-#       when 0
-#         merged.push(right.shift)
-#         sort_whole(left, right, merged)
-#       end
-#     else
-#       merged.push(right.shift)
-#       sort_whole(left, right, merged)
-#     end
-#   elsif right.empty? && left.length.positive?
-#     if left.length > 1
-#       case left[0] <=> left[1]
-#       when 1
-#         merged.push(left.delete_at(1))
-#         sort_whole(left, right, merged)
-#       when -1
-#         merged.push(left.shift)
-#         sort_whole(left, right, merged)
-#       when 0
-#         merged.push(left.shift)
-#         sort_whole(left, right, merged)
-#       end
-#     else
-#       merged.push(left.shift)
-#       sort_whole(left, right, merged)
-#     end
-#   end
-# end
-
 def sort_whole(left, right, merged = [])
   return merged if left.empty? && right.empty?
 
